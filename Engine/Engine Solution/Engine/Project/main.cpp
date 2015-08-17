ed4ae7ceca2c44f03d17d891994104cd78d2d72b
bennybroseph <bennybroseph@gmail.com>
2015-08-17 17:49:39 -0500

    1.0.1
    No changes

--- Engine/Engine Solution/Engine/Project/main.cpp
+++ Engine/Engine Solution/Engine/Project/main.cpp
@@ -8,7 +8,7 @@
 //
 //Spring 2009
 //
-//Edited and Upgraded to SDL 2.0/OpenGL by Ben Odom .
+//Edited and Upgraded to SDL 2.0/OpenGL by Ben Odom 
 //////////////////////////////////////////////////////
 
 #include "Engine/Loops/Loops.h"
